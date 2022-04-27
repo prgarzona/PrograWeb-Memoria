80c09a3000c2559e9f7a5eff7f4cd9f26c130732
Pablo Garzona <prgarzona@gmail.com>
2022-04-26 21:40:33 -0600

    Funcion de revolver cartas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
 
+const pokeCards = [
+  { "src": "/img/004.png" },
+  { "src": "/img/006.png" },
+  { "src": "/img/009.png" },
+  { "src": "/img/150.png" },
+  { "src": "/img/249.png" },
+]
+
+
 function App() {
+
+  //Pokecards aleatorias
+  const revolverCartas = () => {
+    const cartasAletorias = [...pokeCards, ...pokeCards]
+      .sort(() => Math.random() - 0.5)
+      .map((cartas) => ({...cartas,id: Math.random}))
+  }
+
   return (
     <div className="App">
       <h1>Juego Memoria</h1>
